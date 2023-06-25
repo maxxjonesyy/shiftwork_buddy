1fb6c35a49583081e81072197fe3bf6111fa453d
Maxwell Jones <maxxjonesyy@gmail.com>
2023-06-25 14:11:39 +1000

    add pay cycle toggling

--- src/pages/Shifts/ShiftsSummary.jsx
+++ src/pages/Shifts/ShiftsSummary.jsx
@@ -1,39 +1,55 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../../firebase";
 import moment from "moment/moment";
 
 function ShiftsSummary() {
   const { user, shifts } = useContext(UserContext);
 
-  function calculateHours() {
-    const hoursArray = [];
-    shifts.forEach((shift) => {
-      const startTime = moment(shift.start, "hh:mm");
-      const finishTime = moment(shift.finish, "hh:mm");
-      const hoursDiff = finishTime.diff(startTime, "hours");
+  let totalHours = 0;
+  let grossIncome = 0;
 
-      if (hoursDiff < 0) {
-        hoursArray.push(startTime.diff(finishTime, "hours") - 7);
-      } else {
-        hoursArray.push(finishTime.diff(startTime, "hours") - 0.25);
-      }
-    });
+  function getHours(start, finish) {
+    let hours = 0;
+    const startTime = moment(start, "hh:mm");
+    const finishTime = moment(finish, "hh:mm");
+    const hoursDiff = finishTime.diff(startTime, "hours");
 
-    return hoursArray.length > 0
-      ? hoursArray.reduce((prev, curr) => prev + curr)
-      : null;
+    if (hoursDiff < 0) {
+      hours = hoursDiff + 24 - 0.5;
+    } else {
+      hours = hoursDiff - 0.25;
+    }
+    return hours;
   }
 
-  const grossIncome = calculateHours() * user.rate;
-  const yearlyIncome = grossIncome * 26;
+  function getPay(shift) {
+    return getHours(shift.start, shift.finish) * shift.rate;
+  }
 
-  function calculateTax() {
+  function setFrequency(input) {
+    const userRef = doc(db, "users", user.uid);
+    setDoc(userRef, { cycle: input }, { merge: true });
+  }
+
+  function getTax() {
+    let yearlyIncome = 0;
+
+    if (user.cycle === "weekly") {
+      yearlyIncome = grossIncome * 52;
+    } else if (user.cycle === "fortnightly") {
+      yearlyIncome = grossIncome * 26;
+    } else {
+      yearlyIncome = grossIncome * 12;
+    }
+
     const taxFree = 18000;
     const lowBracket = ((Math.floor(45000 - 18201) / 26) * 19) / 100;
     const highBracket = (Math.floor((120000 - 45001) / 26) * 32.5) / 100;
 
     if (yearlyIncome < 18200) {
-      return false;
+      return null;
     } else if (yearlyIncome >= 18201 && yearlyIncome <= 45000) {
       const nineteenCents = Math.floor(
         (((yearlyIncome - taxFree) / 26) * 19) / 100
@@ -44,30 +60,50 @@ function ShiftsSummary() {
         (((yearlyIncome - 45000) / 26) * 32.5) / 100
       );
       return Math.floor(lowBracket + thirtyTwoCents);
-    } else {
+    } else if (yearlyIncome >= 120001 && yearlyIncome <= 180000) {
       const thirtySevenCents =
         ((Math.floor(yearlyIncome - 120000) / 26) * 37) / 100;
       return Math.floor(lowBracket + highBracket + thirtySevenCents);
+    } else {
     }
   }
 
-  calculateTax();
+  shifts.forEach((shift) => {
+    totalHours += getHours(shift.start, shift.finish);
+    grossIncome += getPay(shift);
+  });
 
   const style = {
     container: "py-10 lg:max-w-[75rem] mx-auto",
-    h1: "font-semibold text-lg border-b border-gray-300 pb-2",
+    h1: "font-semibold text-lg ",
     innerContainer: "pt-5",
     netPay: "font-semibold text-[#6d66fa]",
+    cycle:
+      "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-[150px] md:w-[200px] p-2",
+    headingContaner:
+      "flex w-full justify-between border-b border-gray-300 pb-2",
   };
 
   return shifts.length > 0 ? (
     <div className={style.container}>
-      <h1 className={style.h1}>Shift Summary:</h1>
+      <div className={style.headingContaner}>
+        <h1 className={style.h1}>Shift Summary:</h1>
+        <select
+          id='paycycle'
+          className={style.cycle}
+          onChange={() => setFrequency(event.target.value)}
+        >
+          <option>Selected: {user.cycle}</option>
+          <option value='weekly'>Weekly</option>
+          <option value='fortnightly'>Fortnightly</option>
+          <option value='monthly'>Monthly</option>
+        </select>
+      </div>
       <div className={style.innerContainer}>
-        <p>Total hours: {calculateHours()}</p>
+        <p>Total hours: {totalHours}</p>
         <p>Gross pay: ${grossIncome}</p>
-        <p>Tax paid: {calculateTax() ? `-$${calculateTax()}` : `-$${0}`}</p>
-        <p className={style.netPay}>Net pay: ${grossIncome - calculateTax()}</p>
+        <p>Tax paid: {getTax() ? `-$${getTax()}` : `-$${0}`}</p>
+        <p className={style.netPay}>Net pay: ${grossIncome - getTax()}</p>
       </div>
     </div>
   ) : null;
