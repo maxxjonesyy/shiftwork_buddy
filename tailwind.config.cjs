f731812eb9b04700d11a9cb79a27669ccfbf6efb
Maxwell Jones <maxxjonesyy@gmail.com>
2024-07-05 12:46:21 +1000

    change website font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
       colors: {
         primaryBlue: "#3f3d55",
         accentBlue: "#6d66fa",
