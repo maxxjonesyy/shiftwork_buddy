00d5b22ae56e52cdb9fb00295abfcf6b734c0a3b
Max Jones <maxxjonesyy@gmail.com>
2024-07-06 11:05:50 +1000

    create components for rendering alerts

---
+++ src/utils/renderAlert.jsx
@@ -0,0 +1,22 @@
+import Swal from "sweetalert2";
+
+function renderAlert(icon, message) {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    color: "#4d4d4d",
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    },
+  });
+  Toast.fire({
+    icon: icon,
+    title: message,
+  });
+}
+
+export default renderAlert;
