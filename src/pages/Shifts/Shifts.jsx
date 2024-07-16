11da13466a500d6e039900074caf2ae51069b8a7
Max Jones <maxxjonesyy@gmail.com>
2024-07-16 20:55:32 +1000

    refactor shifts component

--- src/pages/Shifts/Shifts.jsx
+++ src/pages/Shifts/Shifts.jsx
@@ -31,28 +31,25 @@ function Shifts() {
   }, []);
 
   const categorizeShifts = (shifts) => {
-    const oldShifts = [];
-    const nowShifts = [];
-    const farShifts = [];
+    const doneShifts = [];
+    const currentShifts = [];
 
     shifts.forEach((shift) => {
       const now = new Date();
       const shiftDate = new Date(shift.convertedDate);
       const diffInDays = (shiftDate - now) / (1000 * 60 * 60 * 24);
 
-      if (shift.checked) {
-        oldShifts.push(shift);
-      } else if (diffInDays > 7) {
-        farShifts.push(shift);
+      if (shift.checked || diffInDays <= -1) {
+        doneShifts.push(shift);
       } else {
-        nowShifts.push(shift);
+        currentShifts.push(shift);
       }
     });
 
-    return { oldShifts, nowShifts, farShifts };
+    return { doneShifts, currentShifts };
   };
 
-  const { oldShifts, nowShifts, farShifts } = categorizeShifts(shifts);
+  const { doneShifts, currentShifts } = categorizeShifts(shifts);
 
   const toggleCollapse = (task) => {
     document.getElementById("collapse-icon").classList.toggle("rotate-90");
@@ -64,32 +61,31 @@ function Shifts() {
   ) : (
     <div className='relative p-5 h-full'>
       <div className='mx-auto lg:max-w-[75rem]'>
-        <div className='flex items-center justify-between mt-5'>
+        <div className='flex flex-col md:flex-row md:justify-between gap-5 mt-5'>
           <h2 className='text-xl font-semibold'>
-            {shifts.length > 0
-              ? "Current Shifts"
-              : "You have no current shifts"}
+            {shifts.length > 0 ? "Your shifts" : "No shifts found"}
           </h2>
-          <div className='flex flex-col md:flex-row'>
+
+          <div className='flex items-center gap-3'>
             <button
               ref={createShiftRef}
-              className='w-32 p-1 text-base text-white rounded-md bg-primaryBlue'>
+              className='px-4 py-2 text-sm text-white rounded-md transition-colors duration-300 bg-primaryBlue hover:bg-accentBlue'>
               Create Shift
             </button>
 
             <button
-              className='w-32 p-1 text-base text-white rounded-md  mt-3 bg-primaryBlue md:ml-5 md:mt-0'
+              className='px-4 py-2 text-sm text-white rounded-md transition-colors duration-300 bg-primaryBlue hover:bg-accentBlue'
               onClick={() => api.deleteAllShifts(user, shifts)}>
               Delete All
             </button>
           </div>
         </div>
 
-        {oldShifts?.length > 0 && (
+        {doneShifts.length > 0 && (
           <ul
-            className='mt-5 overflow-hidden border border-green-200 rounded-md'
+            className='mt-5 overflow-hidden rounded-md shadow-md bg-green-500/60'
             onClick={() => toggleCollapse("completed")}>
-            <div className='flex items-center justify-between p-5 bg-green-100 rounded-md'>
+            <div className='flex items-center justify-between p-4'>
               <h2 className='font-semibold'>Completed Shifts:</h2>
               <img
                 id='collapse-icon'
@@ -100,18 +96,13 @@ function Shifts() {
               />
             </div>
 
-            <div id='completed-shifts' className='hidden'>
-              {oldShifts.map((shift, index) => {
+            <div id='completed-shifts' className='hidden p-5'>
+              {doneShifts.map((shift, index) => {
                 return (
-                  <li
-                    className={
-                      shift.checked ? "shift--checked" : "shift--unchecked"
-                    }
-                    key={index}
-                    id={`ul-${index}`}>
+                  <li className='shift--done' key={index} id={`ul-${index}`}>
                     <input
                       type='checkbox'
-                      checked={shift.checked ? true : false}
+                      checked={true}
                       className='w-5 h-5 accent-[#3f3d55]'
                       onChange={() => api.setChecked(user, shift)}
                     />
@@ -134,16 +125,11 @@ function Shifts() {
           </ul>
         )}
 
-        {nowShifts?.length > 0 && (
+        {currentShifts?.length > 0 && (
           <ul className='mt-5'>
-            {nowShifts.map((shift, index) => {
+            {currentShifts.map((shift, index) => {
               return (
-                <li
-                  className={
-                    shift.checked ? "shift--checked" : "shift--unchecked"
-                  }
-                  key={index}
-                  id={`ul-${index}`}>
+                <li className='shift--unchecked' key={index} id={`ul-${index}`}>
                   <input
                     type='checkbox'
                     checked={shift.checked ? true : false}
@@ -165,55 +151,6 @@ function Shifts() {
                 </li>
               );
             })}
-          </ul>
-        )}
-
-        {farShifts?.length > 0 && (
-          <ul
-            className='mt-5 overflow-hidden border border-orange-200 rounded-md'
-            onClick={() => toggleCollapse("upcoming")}>
-            <div className='flex items-center justify-between p-5 bg-orange-100 rounded-md'>
-              <h2 className='font-semibold'>Upcoming Shifts:</h2>
-              <img
-                id='collapse-icon'
-                className='transition-all'
-                src={collapseIcon}
-                alt='collapse icon'
-                width={25}
-              />
-            </div>
-
-            <div id='upcoming-shifts' className='hidden'>
-              {farShifts.map((shift, index) => {
-                return (
-                  <li
-                    className={
-                      shift.checked ? "shift--checked" : "shift--unchecked"
-                    }
-                    key={index}
-                    id={` -${index}`}>
-                    <input
-                      type='checkbox'
-                      checked={shift.checked ? true : false}
-                      className='w-5 h-5 accent-[#3f3d55]'
-                      onChange={() => api.setChecked(user, shift)}
-                    />
-                    <span className='text-sm md:text-base w-1/2 text-left ml-12'>
-                      {shift.day} {shift.date} {shift.month}
-                    </span>
-                    <span className='text-sm md:text-base w-1/2 text-left ml-12'>
-                      {shift.start} - {shift.finish}
-                    </span>
-                    <img
-                      src={closeIcon}
-                      alt='delete icon'
-                      className='w-5 hover:cursor-pointer'
-                      onClick={() => api.deleteShift(user, shift)}
-                    />
-                  </li>
-                );
-              })}
-            </div>
           </ul>
         )}
       </div>
